d579c86782e08e9dca23bb9f1167908eaceae633
snehal.patra <snehal.patra@emids.com>
2023-03-13 09:31:38 +0530

    Added debugging snippets with in-depth concepts

--- practice41.js
+++ practice41.js
@@ -1,1 +1,10 @@
-let 
+function printHello() {
+    for (let i=1; i<=5; i++) {
+        let a = setTimeout(() => {
+            console.log("Hello! "+i);
+        }, 5000);
+        // console.log(a);
+    }
+}
+
+printHello();
---
+++ practice43.js
@@ -0,0 +1,7 @@
+// Debugging snippet
+var arr1 = [1, 2, 3, 4, 5];
+for (let i=0; i < arr1.length; i++) {
+    setTimeout(() => {
+        console.log(arr1[i]);
+    }, 2000);
+}
